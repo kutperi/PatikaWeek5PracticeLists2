e264333ba77c868f27f6b07a0974ba2d045eb8e9
Bilal <bilalbilin12@gmail.com>
2024-08-12 17:54:53 +0300

    Add project files.

---
+++ PatikaWeek5PracticeLists2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+List<string> coffees = new List<string>(); // Creating an empty list
+
+Console.WriteLine("Lütfen listenizde olmasını istediğiniz 5 kahveyi yazınız: "); // Asking user to fill the list
+
+for (int i = 0; i < 5; i++)
+{
+    coffees.Add(Console.ReadLine()); // Using Add method to add elements to the list
+}
+
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Girilen kahve isimleri: "); 
+
+foreach (string coffee in coffees) // Printing the elements of the list
+{
+    Console.WriteLine(coffee);
+}
